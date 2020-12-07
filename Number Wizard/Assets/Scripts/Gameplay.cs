5f13b1a4f4fd5e7a8dd52a5d5110cf43a6c020ca
CankivancYigit <CankivancYigit@github.com>
2020-12-08 01:05:44 +0300

    Number Wizard

---
+++ Number Wizard/Assets/Scripts/Gameplay.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Gameplay : MonoBehaviour
+{
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    
+    int guess;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+      
+    }
+
+    // Update is called once per frame
+
+    public void OnPressHigher()
+    {
+
+        min = guess + 1;
+        NextGuess();
+
+    }public void OnPressLower()
+    {
+
+        max = guess - 1;
+        NextGuess();
+
+    }
+    void NextGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+
+    }
+}
+
